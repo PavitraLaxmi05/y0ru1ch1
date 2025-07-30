286d80d8a1aaf8e2b9613d69ae3d6ca5294a52b3
PavitraLaxmi05 <n210036@rguktn.ac.in>
2025-07-30 08:42:40 +0530

    modified picoctf post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753678775',resources: [
+    cacheName: 'chirpy-1753845128',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
